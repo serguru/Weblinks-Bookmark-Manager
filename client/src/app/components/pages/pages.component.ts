037a7a457db8b95467ea96fd088add0b4a3dc14c
Sergey Chernyshov <serguru@gmail.com>
2024-12-27 16:16:46 -0800

    Moved data to a service

--- client/src/app/components/pages/pages.component.ts
+++ client/src/app/components/pages/pages.component.ts
@@ -21,33 +21,16 @@ export class PagesComponent implements OnInit {
   constructor(private pagesService: PagesService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    debugger;
-    this.getPages();
-  }
-
-  getPages(){
-    this.pagesService.getPages().subscribe(x => {
+    this.pagesService.pages$.subscribe(x => {
       this.pages = x;
-      if (!this.pages || this.pages.length === 0) {
-        this.selectedPage = null;
-        this.pageMode = PageMode.Add;
-        return;
-      }
-      let path = this.route.snapshot.paramMap.get('path');
-      if (!path) {
-        this.selectedPage = this.pages[0];
-        this.pageMode = PageMode.Browse;
-        return;
-      }
-      path = path.toLowerCase();
-      let page = this.pages.find(x => x.pagePath && x.pagePath.toLowerCase() === path) || null;
-      if (!page) {
-        this.selectedPage = null;
-        this.pageMode = PageMode.NotFound;
-        return;
-      }
-      this.selectedPage = page;      
-      this.pageMode = PageMode.Browse;
     });
+    this.pagesService.selectedPage$.subscribe(x => {
+      this.selectedPage = x;
+    });
+    this.pagesService.pageMode$.subscribe(x => {
+      this.pageMode = x;
+    });
+    const path = (this.route.snapshot.paramMap.get('path') || "").toLowerCase();
+    this.pagesService.getPages(path);
   }
 }
--- client/src/app/services/pages.service.ts
+++ client/src/app/services/pages.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { PageModel } from '../models/PageModel';
+import { PageMode } from '../enums';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,50 @@ export class PagesService {
 
   constructor(private http: HttpClient) { }
 
-  getPages(): Observable<PageModel[]> {
-    return this.http.get<PageModel[]>(`${this.apiUrl}`);
+  private pagesSubject = new BehaviorSubject<any>(null);
+  public pages$ = this.pagesSubject.asObservable();
+
+  private pageModeSubject = new BehaviorSubject<any>(null);
+  public pageMode$ = this.pageModeSubject.asObservable();
+
+  private selectedPageSubject = new BehaviorSubject<PageModel | null>(null);
+  public selectedPage$ = this.selectedPageSubject.asObservable();
+
+  updatePages(newPages: any) {
+    this.pagesSubject.next(newPages);
+  }
+
+  updateSelectedPage(page: any) {
+    this.selectedPageSubject.next(page);
+  }
+
+  updatePageMode(newMode: any) {
+    this.pageModeSubject.next(newMode);
+  }
+
+  getPages(path: string) {
+    this.http.get<PageModel[]>(`${this.apiUrl}`)
+      .subscribe(pages => {
+        this.updatePages(pages);
+        if (!pages || pages.length === 0) {
+          this.updateSelectedPage(null)
+          this.updatePageMode(PageMode.Add);
+          return;
+        }
+        if (!path) {
+          this.updateSelectedPage(pages[0])
+          this.updatePageMode(PageMode.Browse);
+          return;
+        }
+        path = path.toLowerCase();
+        let page = pages.find(x => x.pagePath && x.pagePath.toLowerCase() === path) || null;
+        if (!page) {
+          this.updateSelectedPage(null)
+          this.updatePageMode(PageMode.NotFound);
+          return;
+        }
+        this.updateSelectedPage(page)
+        this.updatePageMode(PageMode.Browse);
+      });
   }
 }
