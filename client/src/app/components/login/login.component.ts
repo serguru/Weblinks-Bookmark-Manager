d80ec9649d9bafd130642dd86fca5058d1878b84
Sergey Chernyshov <serguru@gmail.com>
2024-12-30 15:39:38 -0800

    Mat login form started working

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,29 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  email: string = '';
-  password: string = '';
+// export class LoginComponent {
+//   email: string = '';
+//   password: string = '';
+
+//   constructor(
+//     public loginService: LoginService,
+//     private router: Router
+//   ) { }
+
+//   onSubmit(): void {
+//     this.loginService.login(this.email, this.password)
+//       .subscribe({
+//         next: () => {
+//           this.router.navigate(['/']);
+//         },
+//         error: (error) => {
+//           console.error('Login failed:', error);
+//         }
+//       });
+//   }
+// }
+
+
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  hidePassword = true;
 
   constructor(
+    private fb: FormBuilder,
     public loginService: LoginService,
     private router: Router
-  ) {}
+
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void { }
 
   onSubmit(): void {
-    this.loginService.login(this.email, this.password)
+    if (!this.loginForm.valid) {
+      return;
+    }
+    this.loginService.login(this.loginForm.get("email")!.value, this.loginForm.get("password")!.value)
       .subscribe({
         next: () => {
           this.router.navigate(['/']);
