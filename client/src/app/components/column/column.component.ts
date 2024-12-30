377148882d00f582d41b707d56c22046e6405ce7
Sergey Chernyshov <serguru@gmail.com>
2024-12-30 12:08:00 -0800

    UI looks good

--- client/src/app/components/column/column.component.ts
+++ client/src/app/components/column/column.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { LcolumnModel } from '../../models/LcolumnModel';
 import { LinkComponent } from '../link/link.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-column',
-  imports: [CommonModule, LinkComponent],
+  imports: [CommonModule, LinkComponent, MatCardModule],
   templateUrl: './column.component.html',
   styleUrl: './column.component.css'
 })
