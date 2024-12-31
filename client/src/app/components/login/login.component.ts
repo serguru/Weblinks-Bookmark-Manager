100f7bc6a9c4d6f4b972752f0502da15103d1824
Sergey Chernyshov <serguru@gmail.com>
2024-12-30 17:03:21 -0800

    Login form started working

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -8,6 +8,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { finalize } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
@@ -18,47 +22,28 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-// export class LoginComponent {
-//   email: string = '';
-//   password: string = '';
-
-//   constructor(
-//     public loginService: LoginService,
-//     private router: Router
-//   ) { }
-
-//   onSubmit(): void {
-//     this.loginService.login(this.email, this.password)
-//       .subscribe({
-//         next: () => {
-//           this.router.navigate(['/']);
-//         },
-//         error: (error) => {
-//           console.error('Login failed:', error);
-//         }
-//       });
-//   }
-// }
-
-
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   hidePassword = true;
+  isLoading = false;
 
   constructor(
     private fb: FormBuilder,
     public loginService: LoginService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
 
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
 
@@ -68,14 +53,34 @@ export class LoginComponent implements OnInit {
     if (!this.loginForm.valid) {
       return;
     }
+    this.isLoading = true;
     this.loginService.login(this.loginForm.get("email")!.value, this.loginForm.get("password")!.value)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
       .subscribe({
         next: () => {
           this.router.navigate(['/']);
         },
         error: (error) => {
-          console.error('Login failed:', error);
+          if (error.status === 401) {
+            this.handleLoginError('Invalid email or password');
+          } else {  
+            this.handleLoginError('Unknowm error. Please try again later.');
+          }
         }
       });
   }
+
+  private handleLoginError(error: string): void {
+    this.snackBar.open(error, 'Close', {
+      duration: 5000,
+      panelClass: ['error-snackbar'],
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+
 }
