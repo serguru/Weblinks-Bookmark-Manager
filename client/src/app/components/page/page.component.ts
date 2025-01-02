82557f41f8877c19f3daea46f237e037bbd62c8c
Sergey Chernyshov <serguru@gmail.com>
2025-01-01 23:45:46 -0800

    Context popup menu started working

--- client/src/app/components/page/page.component.ts
+++ client/src/app/components/page/page.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RowComponent } from '../row/row.component';
 import { PagesService } from '../../services/pages.service';
-import { RouterLinkActive, RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { MatButtonModule } from '@angular/material/button';
+import {CdkContextMenuTrigger, CdkMenuItem, CdkMenu} from '@angular/cdk/menu';
+import { PAGE } from '../../common/constants';
 
 @Component({
   selector: 'app-page',
@@ -12,17 +14,32 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     RouterModule,
     RowComponent,
-    RouterLinkActive,
-    MatButtonModule
+    MatButtonModule,
+    CdkContextMenuTrigger, 
+    CdkMenu, 
+    CdkMenuItem
 ],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
 export class PageComponent implements OnInit {
 
-  constructor(public pagesService: PagesService, public loginService: LoginService) {}
+  constructor(public pagesService: PagesService, public loginService: LoginService, private router: Router) {}
 
   ngOnInit(): void {
+  }
+  
+  onContextMenuOpened(page: any): void {
+    console.log('Context menu opened for page:', page);
+  }
+
+  onContextMenuClosed(): void {
+    console.log('Context menu closed');
+    // Handle the menu closed event here
+  }
+
+  navigateToPage(pagePath: string) {
+    this.router.navigate([PAGE + pagePath]);
   }
 
 }
