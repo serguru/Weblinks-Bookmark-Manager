022eab865257a4ad0e64dbbfb72dafb13eb48663
Sergey Chernyshov <serguru@gmail.com>
2025-02-08 08:45:31 -0800

    Addded the background picture

--- client/scripts/queries.sql
+++ client/scripts/queries.sql
@@ -1,8 +1,11 @@
+use weblinks_db
+go
+
 select *
 from pages a
 left join lrows b on b.pageId = a.Id
 left join lcolumns c on c.rowId = b.id
 left join links l on l.columnId = c.id
 where 
-a.accountId = 5
+a.accountId > 5
 
